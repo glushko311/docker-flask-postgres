0ae2a5dfdb62428103f44727bdf46b187a0f7d03
Kamidashii <kamidashii@gmail.com>
2018-07-25 12:43:01 +0300

    delete comments

--- app/app.py
+++ app/app.py
@@ -14,7 +14,6 @@ DBNAME = 'testdb'
 
 
 app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///students.sqlite3'
 app.config['SQLALCHEMY_DATABASE_URI'] = \
     'postgresql+psycopg2://{user}:{passwd}@{host}:{port}/{db}'.format(
         user=DBUSER,
@@ -28,33 +27,6 @@ app.secret_key = 'foobarbaz'
 
 db = SQLAlchemy(app)
 
-
-# class students(db.Model):
-#     id = db.Column('student_id', db.Integer, primary_key=True)
-#     name = db.Column(db.String(100))
-#     city = db.Column(db.String(50))
-#     addr = db.Column(db.String(200))
-#
-#     def __init__(self, name, city, addr):
-#         self.name = name
-#         self.city = city
-#         self.addr = addr
-# #
-# class User(db.Model):
-#     id = db.Column('id', db.Integer, primary_key=True)
-#     login = db.Column('login', db.String, unique=True, not_null=True)
-#     email = db.Column('email', db.String, unique=True, not_null=True)
-#     password = db.Column('password', db.String, not_null=True)
-#     reg_date = db.Column('reg_date', db.Date, not_null=True)
-#     first_name = db.Column('first_name', db.String)
-#     last_name = db.Column('last_name', db.String)
-#     telephone = db.Column('telephone', db.String)
-#     last_login_date = db.Column('reg_date', db.Date, not_null=True)
-#     def __init__(self, login, email, password):
-#         self.login = login
-#         self.password = password
-#         self.email = email
-#         self.reg_date = datetime.datetime.now()
 
 class User(db.Model):
     id = db.Column('id', db.Integer, primary_key=True)
@@ -111,17 +83,6 @@ class User(db.Model):
             return False
 
 
-# def database_initialization_sequence():
-#     db.create_all()
-#     test_rec = students(
-#             'John Doe',
-#             'Los Angeles',
-#             '123 Foobar Ave')
-#
-#     db.session.add(test_rec)
-#     db.session.rollback()
-#     db.session.commit()
-
 @app.route('/user', methods=['GET'])
 def get_all_users():
     users = User.query.all()
@@ -159,28 +120,9 @@ def create_user():
 @app.route('/smoke', methods=['GET'])
 def smoke():
     resp = Response(json.dumps("OK"), status=200)
-    #resp = Response("OK", status=200)
     resp.headers['Access-Control-Allow-Origin'] = '*'
     resp.headers['Content-Type'] = 'application/json'
     return resp
-    # return Response(json.dumps("OK"), status=200)
-#
-# @app.route('/', methods=['GET', 'POST'])
-# def home():
-#     if request.method == 'POST':
-#         if not request.form['name'] or not request.form['city'] or not request.form['addr']:
-#             flash('Please enter all the fields', 'error')
-#         else:
-#             student = students(
-#                     request.form['name'],
-#                     request.form['city'],
-#                     request.form['addr'])
-#
-#             db.session.add(student)
-#             db.session.commit()
-#             flash('Record was succesfully added')
-#             return redirect(url_for('home'))
-#     return render_template('show_all.html', students=students.query.all())
 
 
 if __name__ == '__main__':
@@ -193,5 +135,5 @@ if __name__ == '__main__':
             time.sleep(2)
         else:
             dbstatus = True
-    # database_initialization_sequence()
+
     app.run(debug=True, host='0.0.0.0')
